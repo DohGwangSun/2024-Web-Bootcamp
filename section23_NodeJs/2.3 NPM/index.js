d053e8edc22adfaee5b7ca55cdf7e57c88081f4e
DohGwangSun <appdoh2007@gmail.com>
2024-03-10 20:01:06 +0900

    section 23 Node.js ended

---
+++ section23_NodeJs/2.3 NPM/index.js
@@ -0,0 +1,13 @@
+// var generateName = require("sillyname");
+
+// import generateName from "sillyname";
+// var sillyName = generateName();
+
+// console.log(`my silly name is ${sillyName}`);
+
+
+import superheroes from "superheroes";
+
+var mySuperHeroName = superheroes.random();
+
+console.log(`my superhero name is ${mySuperHeroName}`);
