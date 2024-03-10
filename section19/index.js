827db3385bd434ea9a054a3c5e9386d0c7129388
DohGwangSun <appdoh2007@gmail.com>
2024-03-10 13:42:11 +0900

    section 19 jQuery ended

---
+++ section19/index.js
@@ -0,0 +1,42 @@
+
+$("button").on("click", function() {
+    $("h1").slideUp().slideDown().animate({opacity: 0.5}, 1000);
+});
+
+
+// $("button").on("click", function() {
+//     $("h1").slideToggle();
+// });
+
+
+// $("button").on("click", function() {
+//     $("h1").fadeToggle();
+// });
+
+
+// $("button").remove();
+
+// $(document).keydown(function(event) {
+//     $("h1").text(event.key);
+// });
+
+
+// $("button").click(function() {
+//     $("h1").css("color", "blue");
+// });
+
+// for (var i = 0; i < 5; i++) {
+//   document.querySelectorAll("button")[i].addEventListener("click", function() {
+//     document.querySelector("h1").style.color = "purple";
+//   });
+// }
+
+
+// $("h1").click(function() {
+//     $("h1").css("color", "purple");
+// });
+
+
+// $("a").attr("href", "https://www.yahoo.com");
+// $("h1").text("Bye");
+// $("button").html("<em>Hey</em>");
